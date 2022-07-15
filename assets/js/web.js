0ac1b015c159f7d6db3319a1349005dfa48c3c75
rahmatsaputra-my-id <rahmatsaputra.my.id@gmail.com>
2022-07-15 22:10:39 +0700

    - updated page speed

--- assets/js/web.js
+++ assets/js/web.js
@@ -1,5 +1,16 @@
 
 $(document).ready(function () {
+    // var progressContainer = document.querySelector(".progress-container");
+    // var progressBar = document.querySelector(".progress-bar");
+    // var body = document.querySelector("body");
+    // var width = 1;
+
+    var headerMobile = document.querySelector("#header-mobile-bar-fixed");
+    var headerDesktop = document.querySelector("#header-bar");
+    // var project_budget = document.querySelector('#project-budget');
+    var current_year = document.querySelector('#current-year');
+    var sticky = headerMobile.offsetTop;
+    var sticky = headerDesktop.offsetTop;
 
     // var id = setInterval(_handlerProgresBar, 25);
 
@@ -38,7 +49,7 @@ $(document).ready(function () {
     $(".icon-close").click(function () {
         $(".header-content").hide()
     })
-    
+
     $("#icon-close").click(function () {
         $(".header-content").hide()
     })
