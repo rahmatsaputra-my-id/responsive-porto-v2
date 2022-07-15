2919a15630a725a1239e5891f327cbb475c7e7f2
rahmat.saputra <rahmat.saputra@salt.co.id>
2022-07-15 13:27:26 +0700

    initial commit

--- assets/js/web.js
+++ assets/js/web.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
 
     // var id = setInterval(_handlerProgresBar, 25);
 
-    project_budget.addEventListener('keyup', function (e) {
-        project_budget.value = formatRupiah(this.value, 'Rp ');
-    });
+    // project_budget.addEventListener('keyup', function (e) {
+    //     project_budget.value = formatRupiah(this.value, 'Rp ');
+    // });
 
     // function _handlerProgresBar() {
     //     if (width >= 100) {
@@ -86,20 +86,20 @@ $(document).ready(function () {
         dots[slideIndex - 1].className += " active";
     }
 
-    function formatRupiah(angka, prefix) {
-        var number_string = angka.replace(/[^,\d]/g, '').toString(),
-            split = number_string.split(','),
-            sisa = split[0].length % 3,
-            rupiah = split[0].substr(0, sisa),
-            ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+    // function formatRupiah(angka, prefix) {
+    //     var number_string = angka.replace(/[^,\d]/g, '').toString(),
+    //         split = number_string.split(','),
+    //         sisa = split[0].length % 3,
+    //         rupiah = split[0].substr(0, sisa),
+    //         ribuan = split[0].substr(sisa).match(/\d{3}/gi);
 
-        if (ribuan) {
-            separator = sisa ? '.' : '';
-            rupiah += separator + ribuan.join('.');
-        }
+    //     if (ribuan) {
+    //         separator = sisa ? '.' : '';
+    //         rupiah += separator + ribuan.join('.');
+    //     }
 
-        rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
-        return prefix == undefined ? rupiah : (rupiah ? 'Rp ' + rupiah : '');
-    }
+    //     rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+    //     return prefix == undefined ? rupiah : (rupiah ? 'Rp ' + rupiah : '');
+    // }
 
 });
